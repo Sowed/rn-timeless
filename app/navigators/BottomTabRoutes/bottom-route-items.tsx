bba6f60de116a4490099e452939682ba8aadb391
Sowed <sowed@cyberdude.com>
2022-05-17 20:46:17 +0400

    feat: Create the `BottomTabRoutes` scaffold
    This navigator holds all routes that are displayed immediately on the
    bottom tab navigation and their respective children.
    - Create the `BottomTabItem` to be used in rendering a custom bottom tabs icon.
    - Setup the `bottom-route-items` that defines the configuration/screens
    and types of the items for the bottom tabs.

---
+++ app/navigators/BottomTabRoutes/bottom-route-items.tsx
@@ -0,0 +1,112 @@
+import React, { ComponentType } from 'react';
+
+import {
+  HomeScreen,
+  SettingsScreen,
+  TradingScreen,
+  WalletScreen,
+} from '@screens';
+
+import { Icon } from '@components';
+
+export type TBottomTabsParamsList = {
+  HomeScreen: undefined;
+  SettingsScreen: undefined;
+  TradingScreen: undefined;
+  WalletScreen: undefined;
+};
+
+export type TBottomTabItems = {
+  name: keyof TBottomTabsParamsList;
+  component: ComponentType<unknown>;
+  options?: Record<string, unknown>;
+};
+
+/**
+ * -----------------------------------------------------------------------------
+ * List the bottom tab items and respective stack
+ * TODO:
+ * - Add a custom icon for each tab, SVG instead of the icon fonts, for finer control
+ * - Let the bottoms tab item extend the options to allow for things like labels/counters.
+ * - Get the focus and label colors from the theme
+ */
+const bottomRouteItems: TBottomTabItems[] = [
+  {
+    name: 'WalletScreen',
+    component: WalletScreen,
+    options: {
+      tabBarLabel: 'Wallet',
+      tabBarIcon: (props: {
+        size: number;
+        focused: boolean;
+        color: string;
+      }) => (
+        <Icon
+          name="credit-card"
+          color={props.color}
+          size={props.size}
+          solid={props.focused}
+        />
+      ),
+    },
+  },
+  {
+    name: 'HomeScreen',
+    component: HomeScreen,
+    options: {
+      tabBarLabel: 'Drops',
+      tabBarIcon: (props: {
+        size: number;
+        focused: boolean;
+        color: string;
+      }) => (
+        <Icon
+          name="diamond"
+          color={props.color}
+          size={props.size}
+          solid={props.focused}
+        />
+      ),
+    },
+  },
+  {
+    name: 'TradingScreen',
+    component: TradingScreen,
+    options: {
+      tabBarLabel: 'Trading',
+      tabBarIcon: (props: {
+        size: number;
+        focused: boolean;
+        color: string;
+      }) => (
+        <Icon
+          name="balance-scale"
+          color={props.color}
+          size={props.size}
+          solid={props.focused}
+        />
+      ),
+    },
+  },
+  {
+    name: 'SettingsScreen',
+    component: SettingsScreen,
+    options: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: (props: {
+        size: number;
+        focused: boolean;
+        color: string;
+      }) => (
+        <Icon
+          name="cog"
+          color={props.color}
+          size={props.size}
+          solid={props.focused}
+        />
+      ),
+    },
+  },
+];
+
+export default bottomRouteItems;
